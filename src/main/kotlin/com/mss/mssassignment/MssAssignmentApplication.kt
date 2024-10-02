452eee2faa585b71dd66594c3e24da83c3b2b678
john.p <parkjuwon0603@gmail.com>
2024-10-02 11:51:07 +0900

    first commit

---
+++ src/main/kotlin/com/mss/mssassignment/MssAssignmentApplication.kt
@@ -0,0 +1,11 @@
+package com.mss.mssassignment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MssAssignmentApplication
+
+fun main(args: Array<String>) {
+    runApplication<MssAssignmentApplication>(*args)
+}
---
+++ src/test/kotlin/com/mss/mssassignment/MssAssignmentApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mss.mssassignment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MssAssignmentApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
